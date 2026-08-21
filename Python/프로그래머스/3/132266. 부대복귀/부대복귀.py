289eb16cf133a2b3e95f61184df88e88b3ac4b9a
0yewon0 <77890982+0yewon0@users.noreply.github.com>
2026-08-21 15:16:33 +0900

    [level 3] Title: 부대복귀, Time: 211.78 ms, Memory: 107 MB -BaekjoonHub

---
+++ Python/프로그래머스/3/132266. 부대복귀/부대복귀.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+def solution(n, roads, sources, destination):
+    graph = [[] for _ in range(n + 1)]
+    
+    for a, b in roads:
+        graph[a].append(b)
+        graph[b].append(a)
+    
+    distance = [-1] * (n + 1)
+    distance[destination] = 0
+    
+    q = deque([destination])
+    
+    while q:
+        now = q.popleft()
+        
+        for next_node in graph[now]:
+            if distance[next_node] == -1:
+                distance[next_node] = distance[now] + 1
+                q.append(next_node)
+    
+    answer = []
+    
+    for source in sources:
+        answer.append(distance[source])
+    
+    return answer
